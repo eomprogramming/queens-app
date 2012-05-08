eb23e6d72731a1e8141f06e6d2bac30718aeb3fa
David Huynh <hynhdvd@gmail.com>
2012-05-08 13:09:57 -0400

    Set up Xcode project.

---
+++ xcode/Q'BetTests/Q_BetTests.h
@@ -0,0 +1,13 @@
+//
+//  Q_BetTests.h
+//  Q'BetTests
+//
+//  Created by David Huynh on 12-05-08.
+//  Copyright (c) 2012 EOM. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Q_BetTests : SenTestCase
+
+@end
---
+++ xcode/Q'BetTests/Q_BetTests.m
@@ -0,0 +1,32 @@
+//
+//  Q_BetTests.m
+//  Q'BetTests
+//
+//  Created by David Huynh on 12-05-08.
+//  Copyright (c) 2012 EOM. All rights reserved.
+//
+
+#import "Q_BetTests.h"
+
+@implementation Q_BetTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Q'BetTests");
+}
+
+@end
